c824d06ea8513a3a74a41e2d8ce2d22400b423a0
ervina <eindivinereinherjar@gmail.com>
2017-06-10 22:07:23 +0700

    add raw arena
    all model rigidbodies doesn't use gravity
    
    todo:
    . make fence when i've figured out the sizing

---
+++ game/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[SerializeField] private GameObject arenaCircle;
+
+	public GameObject ArenaCircle {
+		get {
+			return arenaCircle;
+		}
+	}
+		
+	// Use this for initialization
+	void Start () {
+		arenaCircle.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BuildArena() {
+		arenaCircle.SetActive (true);
+	}
+}
