d6d6ab98777f8f16fb390b9850e6a383137fe678
Vallenteer <kristiantokelvin@yahoo.co.id>
2017-07-26 20:51:23 +0700

    Buat Menu lebih menarik

---
+++ game/Assets/Arts/UI_MENU/ScrollingBackgrounf/BackgroundBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBehavior : MonoBehaviour {
+
+	public float Speed = 5f;
+	void Update () {
+
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		if (offset.x >= 10)
+			offset.x = 0;
+		offset.x += Time.deltaTime / Speed; // beda sma BG1 itu dri speed aj .-.
+
+		mat.mainTextureOffset = offset;
+
+	}
+}
